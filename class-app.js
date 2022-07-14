a4be594fd768b26b2784202ff4e7884c2b8ca42e
Robert Ball <bobball89@gmail.com>
2022-07-14 10:45:40 -0700

    updated array in question 7 js

--- class-app.js
+++ class-app.js
@@ -46,3 +46,43 @@ for(let i = 0; i < 4; i++){
 if(i === 3){
   alert('The answer was 30');
 }
+
+
+// Question number seven array attempt
+
+let favMovies = ['the rock', 'con air', 'big trouble in little china', 'king arthur', 'lord of the rings trilogy', 'hackers', 'crimson tide', 'the hunt for red october'];
+
+// // setting conditions for correct guess
+// // I can't figure out how to stucture the array in the alert without using the .join() helper
+
+for(let i = 0; i<6; i++){//slow loop
+  let favMoviesGuess = prompt('Guess one of my fav movies').toLowerCase();
+
+  for(let j = 0; j < favMovies.length; j++){// fast loop
+    if(favMoviesGuess === favMovies[j]){
+      alert('That is right');
+      i = 6;
+      break;
+    }
+  }
+
+}
+
+alert('Your score is + scoreCount ');
+
+// let correctAnswer = false;
+// for (let i = 0; i < 6; i++) {
+//   if(correctAnswer){
+//     break;
+//   } else {
+//     let userMovieGuess = prompt('Guess one of my top 10 favorite movies, you have 6 chances to get this!').toLowerCase();
+//     for (let i = 0; i < favMovies.length; i++){
+//       if (favMovies[i] === userMovieGuess){
+//         alert(`That is one of my favorite movies ${user}, here is the whole list \n${favMovies}!`);
+//         score++;
+//         correctAnswer = true;
+//         break;
+//       }
+//     }
+//   }
+// }
--- js/app.js
+++ js/app.js
@@ -81,7 +81,7 @@ for (let i = 0; i < 4; i++){
 
 // // Guessing game using array of favorite movies as potential answers
 
-let favMovies = ['the rock', 'con air', 'big trouble in little china', 'king arthur', 'lord of the rings trilogy', 'hackers', 'crimson tide', 'the hunt for red october'];
+let favMovies = ['the rock', 'con air', 'big trouble in little china', 'king arthur', 'lord of the rings trilogy', 'hackers', 'crimson tide', 'the fast and the furious', 'the hunt for red october'];
 
 // // setting conditions for correct guess
 // // I can't figure out how to stucture the array in the alert without using the .join() helper
@@ -94,7 +94,7 @@ for (let i = 0; i < 6; i++) {
     let userMovieGuess = prompt('Guess one of my top 10 favorite movies, you have 6 chances to get this!').toLowerCase();
     for (let i = 0; i < favMovies.length; i++){
       if (favMovies[i] === userMovieGuess){
-        alert(`That is one of my favorite movies ${user}, here is the whole list \n${favMovies}!`);
+        alert(`That is one of my favorite movies ${user}, here is the whole list \nThe Rock\nCon Air\nBig Trouble in Little China\nKing Arthur\nLord of the Rings trilogy\nHackers\nBloodsport\nCrimson Tide\nThe Fast and the Furious\nThe Hunt for Red October`);
         score++;
         correctAnswer = true;
         break;
